974e31a4e6d0400eea9934d4c9d89bc702517b32
sagar9926 <sagarkumar9926@gmail.com>
2020-11-01 22:14:14 +0530

    Create Polygon.py

---
+++ Polygon.py
@@ -0,0 +1,66 @@
+import math
+
+class Polygon :
+    
+    def __init__(self,number_edges,circum_radius):
+        self.edges = number_edges
+        self.circum_rad = circum_radius
+        
+    @property
+    def edges(self):
+        print("Getting Edges...")
+        return self._number_edges
+    
+    @edges.setter
+    def edges(self,number_edges):
+        print("Setting edges value...")
+        if number_edges < 3:
+            raise ValueError("Minimum number of edges required to form a polygon is 3")
+        self._number_edges = number_edges
+        
+    @property
+    def circum_rad(self):
+        print("Getting Cirum radius ...")
+        return self._circum_rad
+    
+    @circum_rad.setter
+    def circum_rad(self,circum_radius):
+        print("Setting circum radius value...")
+        if circum_radius < 0:
+            raise ValueError("Cirum radius cannot be negative")
+        self._circum_rad = circum_radius
+   
+    @property 
+    def interiorAngle(self):
+        return((self.edges - 2)*180 / self.edges)
+
+    @property 
+    def edgeLength(self):
+        return(2*self.circum_rad * math.sin(math.pi/self.edges))
+    
+    @property
+    def apothem(self):
+        return(self.circum_rad*math.cos(math.pi/self.edges))
+        
+    @property
+    def area(self):
+        return(0.5 * self.edges* self.edgeLength * self.apothem)
+    
+    @property
+    def perimeter(self):
+        return(self.edges* self.edgeLength)
+    
+    def __repr__(self):
+        return f'Polygon(Edges = {self.edges}, Circum Radius = {self.circum_rad})'
+    
+    def __eq__(self,other):
+        if isinstance(self,type(other)):
+            return ((self.edges == other.edges) and self.circum_rad == other.circum_rad)
+        else:
+            raise TypeError(f"Cannot compare values of type {self.__class__} and {other.__class__}")
+            
+    def __gt__(self,other):
+        if isinstance(self,type(other)):
+            return (self.circum_rad > other.circum_rad)
+        else:
+            raise TypeError(f"Cannot compare values of type {self.__class__} and {other.__class__}")
